f730e42b42ece1e46b7c6a70de9d00fb9d94ee93
LukasKock <lukaskock@gmail.com>
2023-08-19 18:47:52 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradleCleaner
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog ofg;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofg.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ofg = new OpenFileDialog();
+        }
+    }
+}
